d1bc3a79a5d7e80ff94fbc60648d6627952bb48b
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-02-08 15:01:49 +0100

    test

---
+++ day-2-start/src/js/baas/auth/signupUser.js
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js';
+
+// make a connection to supabase
+
+const supabase = createClient{
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+};
+
+async function signupUser(email, password) {
+  let { data, error } = await supabase.auth.signUp(credentials: {
+    email: email,
+    password: password
+  })
+  if (error) {
+    return error
+
+
+  } else {
+    return data;
+  }
+}
